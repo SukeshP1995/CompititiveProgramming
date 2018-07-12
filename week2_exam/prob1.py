216222026b6986c705a514c331c0957ef11783cd
SukeshP1995 <sukeshpabolu@gmail.com>
2018-07-12 14:39:46 +0530

    week2 exam prob1 added

---
+++ week2_exam/prob1.py
@@ -0,0 +1,20 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..'}
+inputs = [
+          ["gin", "zen", "gig", "msg"],
+          ["a", "z", "g", "m"],
+          ["bhi", "vsv", "sgh", "vbi"],
+          ["a", "b", "c", "d"],
+          ["hig", "sip", "pot"]  
+          ]
+
+for inp_list in inputs:
+     print(inp_list)
+     print(len(set([''.join(MORSE_CODE_DICT[c] for c in inp.upper()) for inp in inp_list])))
